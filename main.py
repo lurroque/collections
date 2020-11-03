8ac9e040e4e1a5bfff9999381a4c62ef775f9993
Roque <luizroque@yacows.com.br>
2020-11-03 12:32:58 -0300

    Entendo diferenças de paradigmas - Funciona e OO

--- main.py
+++ main.py
@@ -12,7 +12,23 @@ conta_2.deposita(200)
 # OBS: Tuplas são imutáveis
 # É possível apenas acessar seus valores
 # t[0]
-
 t = ("Roque", 30, 1990)
 
-print(t)
+
+# Variação funcional
+# O Objeto original não é alterado
+# é possível atribuir esse objetos a novas variáveis,
+# executar operações.
+# E por fim, devolver um novo objeto a partir das operações realizadas.
+conta_roque = (15, 100)
+
+
+def deposita(conta):
+    novo_saldo = conta_roque[1] + 100
+    codigo = conta_roque[0]
+    return (codigo, novo_saldo)
+
+
+valor = deposita(conta_roque)
+print(conta_roque)
+print(valor)
