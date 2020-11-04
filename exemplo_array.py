88a97964dbff2aeaad9afa68ab2ffe7d5dbbd93e
Roque <luizroque@yacows.com.br>
2020-11-04 11:00:11 -0300

    Exemplos de array

---
+++ exemplo_array.py
@@ -0,0 +1,9 @@
+# Exemplos do uso de array dentro da linguagem
+# É preciso importar o array, está na biblioteca padrão da linguagem
+# Seu uso deve ser evitado, o exemplo é apenas por curiosidade
+import array as arr
+
+# Instanciar e informar o tipo do array
+# Um array por padrão, só pode armazenar um tipo de dado
+# d = decimal
+arr.array("d", [1, 3.5])
